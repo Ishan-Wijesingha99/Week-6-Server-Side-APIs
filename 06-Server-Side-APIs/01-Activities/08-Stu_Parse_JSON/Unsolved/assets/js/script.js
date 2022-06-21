ea6ab692a66bb6dc0bae66b2ec29dfcff67393e8
Ishan <you@example.com>
2022-06-21 21:22:30 +1000

    activity 8 completed

--- 06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/08-Stu_Parse_JSON/Unsolved/assets/js/script.js
@@ -1,13 +1,20 @@
 // TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
-var requestUrl = 'https://api.github.com/repos/{owner}/{repo}/issues';
+var requestUrl = 'https://api.github.com/repos/twitter/chill/issues?per_page=5';
+
+// We can change the web API URL to only include certain amounts of information.
+// So for instance, we might not want the ENTIRE JSON object, we might only want a part of it. We can change the URL a bit to only fetch the part we want 
+
+// this is called query params , look into it
+
 
 fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
+.then(response => response.json())
+.then(data => {
     console.log('Github Repo Issues \n----------');
     console.log(data);
-    // TODO: Loop through the response
-    // TODO: Console log each issue's URL and each user's login
+
+    data.forEach(function(object) {
+      console.log(`${object.url}`)
+      console.log(`${object.user.login}`)
+    })
   });
