8ab922f5a4be9ea384a19653216ae6ceb442aa76
Ishan <ishanwijes@gmail.com>
2022-06-23 19:43:51 +1000

    activity 14 completed

--- 06-Server-Side-APIs/01-Activities/13-Ins_Fetch_Status/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/13-Ins_Fetch_Status/assets/js/script.js
@@ -2,6 +2,11 @@ var requestUrl = 'https://api.github.com/orgs/nodejs/repos?per_page=5';
 
 var responseText = document.getElementById('response-text');
 
+
+
+// the response object has a property which is response.status , this is the status code
+// status code of 200 means fetch request was successful
+
 function getApi(requestUrl) {
   fetch(requestUrl)
     .then(function (response) {
--- 06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
@@ -1,18 +1,22 @@
-var badRequestUrl = 'https://api.github.com/orgs/nodejs/oreps?per_page=5';
+
+// deliberately create wrong URL 
+var badRequestUrl = 'https://yeet.github.com/orgs/nodejs/oreps?per_page=5';
 
 var responseText = document.getElementById('response-text');
 
+
 function getApi(request) {
-  fetch(requestUrl)
-    .then(function (response) {
-      // Check the console first to see the response.status
-      console.log(response.status);
-      // Then write the conditional based on that response.status value
-      // Make sure to display the response on the page
-    })
-    .then(function (data) {
+  fetch(request)
+  .then(response => {
+    
+      if(response.status !== 200) {
+        responseText.textContent = response.status
+      }
+      return response.json()
+  })
+  .then(data => {
       console.log(data);
-    });
+  });
 }
 
 getApi(badRequestUrl);
