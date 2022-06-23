d5380dccad9346683b249a6417de9a9990f3885b
Ishan <ishanwijes@gmail.com>
2022-06-23 20:26:22 +1000

    acitvity 16 completed

--- 06-Server-Side-APIs/01-Activities/15-Ins_Deconstruct_Parameters/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/15-Ins_Deconstruct_Parameters/assets/js/script.js
@@ -10,10 +10,4 @@ fetch('https://api.github.com/gists/public?since=2020-06-01&per_page=100')
 // deconstruct parameters , query string parameters in URL for fetch
 
 
-// git contribution test
-let gitContributionTest1;
-let gitContributionTest2;
-let gitContributionTest3;
-let gitContributionTest4;
-
 
